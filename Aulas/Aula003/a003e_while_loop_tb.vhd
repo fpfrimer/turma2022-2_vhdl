a0b1211b7cb98c9fe59b0f51a3582937ce759885
VitorThimoteoDaubermann <145594435+VitorThimoteoDaubermann@users.noreply.github.com>
2025-05-01 10:11:48 -0300

    Mudanças Vitor Thimóteo
    Comentários no código, está compilando

--- Aulas/Aula003/a003e_while_loop_tb.vhd
+++ Aulas/Aula003/a003e_while_loop_tb.vhd
@@ -15,16 +15,20 @@ begin
         variable i  :   integer := 20; -- Inicializa a variável 'i' em 20 para contar regressivamente até 1.
         -- Note que i foi declarado sem range, portanto, é uma variável de 32 bits
     begin
-        report "Start!";
+        report "Start!";-- Indica o início do processo de contagem regressiva
         
+        -- Laço enquanto (while) para iterar enquanto i for maior que zero
         while i > 0 loop
+            -- Condição de saída explícita: interrompe o loop quando i atinge o valor de 'n'
             exit when i = n;
-            report "i = " & integer'image(i);
-            i := i - 1;
-        end loop;
+             -- Report gera uma mensagem de simulação mostrando o valor atual de i
+            report "i = " & integer'image(i); 
+            i := i - 1; -- Atualiza i decrementando em 1 a cada iteração
+        end loop; -- Fim do loop: o código alcança este ponto quando i == n ou i <= 0
 
-        report "Ateh!";
-        wait;
+        report "Ateh!"; -- Indica o fim do processo de contagem ou saída antecipada
+        wait; -- Suspende o processo indefinidamente, finalizando a simulação
     end process;
 
 end architecture;
+    
