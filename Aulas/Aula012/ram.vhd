4447e2af6291dde99ffe0682c7bb9383576388df
fpfrimer <fpfirmer@gmail.com>
2025-06-08 13:45:45 -0300

    memórias

--- Aulas/Aula012/ram.vhd
+++ Aulas/Aula012/ram.vhd
@@ -5,7 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;                   
 use ieee.numeric_std.all;                      
 
-entity single_port_ram_with_init is
+entity ram is
 
 	generic 
 	(
@@ -15,22 +15,22 @@ entity single_port_ram_with_init is
 
 	port 
 	(
-		clk		: in std_logic;                                         -- Clock de entrada
-		addr	: in natural range 0 to 2**ADDR_WIDTH - 1;             -- Endereço de leitura/escrita
-		data	: in std_logic_vector((DATA_WIDTH-1) downto 0);        -- Dado de entrada para escrita
-		we		: in std_logic := '1';                                 -- Sinal de habilitação de escrita (write enable)
-		q		: out std_logic_vector((DATA_WIDTH -1) downto 0)       -- Dado de saída (leitura)
+		clk		: in std_logic;                                     -- Clock de entrada
+		addr	: in unsigned(ADDR_WIDTH - 1 downto 0);             -- Endereço de leitura/escrita
+		data	: in std_logic_vector(DATA_WIDTH - 1 downto 0);     -- Dado de entrada para escrita
+		we		: in std_logic := '1';                              -- Sinal de habilitação de escrita (write enable)
+		q		: out std_logic_vector(DATA_WIDTH - 1 downto 0)     -- Dado de saída (leitura)
 	);
 
-end single_port_ram_with_init;
+end ram;
 
 architecture rtl of single_port_ram_with_init is
 
-	-- Build a 2-D array type for the RAM
-	subtype word_t is std_logic_vector((DATA_WIDTH-1) downto 0);         -- Define o tipo de uma palavra na RAM
+	-- Controi um array de std_logic_vector para compor os elementos da memória
+	subtype word_t is std_logic_vector(DATA_WIDTH - 1 downto 0);         -- Define o tipo de uma palavra na RAM
 	type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;          -- Define o tipo da memória como um array de palavras
 
-	-- Função para inicializar a RAM com o próprio valor do endereço
+	-- Função para inicializar a RAM com o próprio valor do endereço (opcional)
 	function init_ram
 		return memory_t is 
 		variable tmp : memory_t := (others => (others => '0'));         -- Inicializa todas as posições com zeros
@@ -45,10 +45,10 @@ architecture rtl of single_port_ram_with_init is
 	-- Declare the RAM signal and specify a default value.	Quartus Prime
 	-- will create a memory initialization file (.mif) based on the 
 	-- default value.
-	signal ram : memory_t := init_ram;            -- Instancia a RAM com conteúdo inicial
+	signal ram : memory_t := init_ram();            -- Instancia a RAM com conteúdo inicial
 
 	-- Register to hold the address 
-	signal addr_reg : natural range 0 to 2**ADDR_WIDTH-1;   -- Registrador para armazenar o endereço lido
+	signal addr_reg : unsigned(ADDR_WIDTH - 1 downto 0);   -- Registrador para armazenar o endereço lido
 
 begin
 
@@ -56,7 +56,7 @@ begin
 	begin
 	if(rising_edge(clk)) then                     -- Executa na borda de subida do clock
 		if(we = '1') then                         -- Verifica se escrita está habilitada
-			ram(addr) <= data;                    -- Escreve o dado no endereço especificado
+			ram(to_integer(addr)) <= data;                    -- Escreve o dado no endereço especificado
 		end if;
 
 		-- Register the address for reading
@@ -64,6 +64,6 @@ begin
 	end if;
 	end process;
 
-	q <= ram(addr_reg);                           -- Atribui à saída o conteúdo lido da RAM
+	q <= ram(to_integer(addr));                           -- Atribui à saída o conteúdo lido da RAM
 
 end rtl;
